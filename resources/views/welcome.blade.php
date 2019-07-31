edfa0894fa45e31656917efdac62a9a596c93d2c
fazaaaa <mfazafadhilah96@gmai.com>
2019-07-31 08:02:14 +0700

    hayu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Selamat Datang
                 </div>
 
                 <div class="links">
