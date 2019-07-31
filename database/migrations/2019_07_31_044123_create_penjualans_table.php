fdbf50dd9b3a10111f16fd5317fafab9540cb8cb
fazaaaa <mfazafadhilah96@gmai.com>
2019-07-31 12:44:02 +0700

    hayuu

---
+++ database/migrations/2019_07_31_044123_create_penjualans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePenjualansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('bukus_id');
+            $table->bigIncrements('kasirs_id');
+            $table->integer('jumlah');
+            $table->integer('total');
+            $table->date('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjualans');
+    }
+}
